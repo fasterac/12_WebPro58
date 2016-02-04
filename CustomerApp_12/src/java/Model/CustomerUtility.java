aba629624d7f1ecd3a61b0a1de34a361cef0f150
Boontarika Pochchongdach <fasterac@users.noreply.github.com>
2016-02-05 01:28:50 +0900

    Model package desune
    อย่าลืม refactor นะจ๊ะ ไม่น่าเลย T_T

--- CustomerApp_12/src/java/Model/CustomerUtility.java
+++ CustomerApp_12/src/java/Model/CustomerUtility.java
@@ -1,8 +1,14 @@
-package model;
+package Model;
 
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import Model.Customer;
 
 public class CustomerUtility {
     
@@ -12,54 +18,44 @@ public class CustomerUtility {
     public void init() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://database.it.kmitl.ac.th:3306/it_12";
-            String user = "it_12";
-            String pwd = "ust4Ht3Q";
-            conn = DriverManager.getConnection(url,user,pwd);
+            conn = DriverManager.getConnection("jdbc:mysql://database.it.kmitl.ac.th:3306/it_12", "it_12", "ust4Ht3Q");
             stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerUtility.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(CustomerUtility.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(CustomerUtility.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public int insertData(String firstName, String lastName, String mobile, String email, String addr){
-        String sql = "INSERT INTO CustomerMVC VALUES(0, ?, ?, ?, ?, ? ,?)";
-        PreparedStatement preparedStatement = conn.prepareStatement(sql);
-        preparedStatement.setString(1, firstName);
-        preparedStatement.setString(2, lastName);
-        preparedStatement.setString(3, company);
-        preparedStatement.setString(4, mobile);
-        preparedStatement.setString(5, email);
-        preparedStatement.setString(6, addr);
-
-        preparedStatement.executeUpdate(); // execute the sql script (update)
-        preparedStatement.close(); // close statement (optional for security)
-
-
-        ResultSet resultSet = preparedStatement.executeQuery(); // execute the sql script (query - search)
-        resultSet.last(); // set cursor to the last row of result
-
-        return resultSet.getInt("id");
-    }
-    
-    
-    
-    public ResultSet getData(){
-        String username = null;
-        String sql = "SELECT * FROM CustomerMVC";
-        ResultSet rs = null;
-        
+    public int insertData(Customer cus){
+        String sqlCmd = 
+               "insert into mvccustomer values('" + cus.getFirstName() + 
+               "','" + cus.getLastName() + 
+                "','" + cus.getCompany()+ 
+               "','" + cus.getMobile() + 
+               "','" + cus.getEmail() + 
+               "'," + cus.getAddr() + "');";
+       
+        int num = 0;
         try {
-            
-            pstmt = conn.prepareStatement(sql);
-            rs =  pstmt.executeQuery();
-                       
+            num = stmt.executeUpdate(sqlCmd);
         } catch (SQLException ex) {
             Logger.getLogger(CustomerUtility.class.getName()).log(Level.SEVERE, null, ex);
         }
-         return rs;
-        
+        return num;
     }
+    
+    /*public void getData(){
+        try {
+            String sql = "SELECT * FROM CustomerMVC WHERE firstname = ? AND lastname = ?";
+            PreparedStatement stmt = Database.getInstance().getConnection().prepareStatement(sql);
+            stmt.setString(1, firstname);
+            stmt.setString(2, lastname);
+
+            return queryCustomer(stmt);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }*/
 }
