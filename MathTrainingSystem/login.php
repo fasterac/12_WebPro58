02374055d8a74069bed51a3c6eee081105c64faa
leloucid <s7070149@kmitl.ac.th>
2016-04-21 16:01:08 +0700

    login.php
    new login

--- MathTrainingSystem/login.php
+++ MathTrainingSystem/login.php
@@ -17,7 +17,7 @@
 
     <style>
         body {
-            font-family: 'Cordia New';
+            font-family: 'TH SarabunPSK';
         }
 
         div.container {
@@ -32,33 +32,34 @@
 	<div class="row">
 		<div class="col-md-12">
 		<!--Login-->
-
+			<center>
 			<h1>
 				>> Login
 			</h1>
+			</center>
             <!-- add action="controllerLogin.php" from original login form -> login.php -->
 			<form action="controllerLogin.php" class="form-horizontal" role="form" method="POST">
 				<div class="form-group">
 					 <!-- input email-->
-					<label for="inputEmail3" class="col-sm-2 control-label">
+					<label for="inputEmail3" class="col-sm-4 control-label">
 						Email
 					</label>
-					<div class="col-sm-10">
+					<div class="col-sm-5">
 						<!-- add name="username" for $_POST["username"] matching in controllerLogin.php from original login form -->
 						<input class="form-control" id="inputEmail3" name="username" type="text" />
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="inputPassword3" class="col-sm-2 control-label">
+					<label for="inputPassword3" class="col-sm-4 control-label">
 						Password
 					</label>
-					<div class="col-sm-10">
+					<div class="col-sm-5">
                         <!-- add name="password" for $_POST["password"] matching in controllerLogin.php from original login form -->
 						<input class="form-control" id="inputPassword3" name="password" type="password" />
 					</div>
 				</div>
 				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
+					<div class="col-sm-offset-4 col-sm-5">
 						<div class="checkbox">
 							<label>
 								<input type="checkbox" /> Remember me
@@ -67,7 +68,7 @@
 					</div>
 				</div>
 				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
+					<div class="col-sm-offset-4 col-sm-5">
                         <!-- add name="subbutton", value="login" for $_POST["subbutton"] matching in controller.php from original login form -->
 						<button type="submit" class="btn btn-default">
 							Login
@@ -92,7 +93,7 @@
 						 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
 					</button> <a class="navbar-brand" href="#"> สัญลักษณ์ IT ระบบฝึกทักษะทางคณิตศาสตร์</a>
 				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-5">
 					<ul class="nav navbar-nav navbar-right">
 						<li class="active">
 							<a href="#">home</a>
@@ -100,33 +101,8 @@
 						<li>
 							<a href="#">login</a>
 						</li>
-						<li class="dropdown">
-							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-							<ul class="dropdown-menu">
-								<li>
-									<a href="#">Action</a>
-								</li>
-								<li>
-									<a href="#">Another action</a>
-								</li>
-								<li>
-									<a href="#">Something else here</a>
-								</li>
-								<li class="divider">
-								</li>
-								<li>
-									<a href="#">Separated link</a>
-								</li>
-								<li class="divider">
-								</li>
-								<li>
-									<a href="#">One more separated link</a>
-								</li>
-							</ul>
-						</li>
 					</ul>
-					
-					
+
 				</div>
 				
 			</nav>
